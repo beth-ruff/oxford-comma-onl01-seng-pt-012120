141f6c8d9653239d2b9236138af10255c80fb6e4
Beth Ruff <beth.ruff2@gmail.com>
2020-02-01 01:56:59 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -8,11 +8,11 @@ end
 
 def oxford_common(array)
  if array.length == 2 
-   array.join(" and ")
+  array.join(" and ")
  elsif array.length > 2 
   array.insert(-2, "and")
   array[0..-1].join(", ")
-  else
-    array.join
-  end
+ else
+  array.join
+end
 end
