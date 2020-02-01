1758411696c86eee8f11ac94e4b218bead1997c3
Beth Ruff <beth.ruff2@gmail.com>
2020-02-01 02:36:58 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,20 @@
 def oxford_comma(array)
-
+  array.join
 end
+
+def oxford_comma(array)
+  array.join(" and ")
+end
+
+def oxford_comma(array)
+    if array.size == 2 
+      array.join(" and ")
+    elsif array.size > 2 
+      last_word = array.pop
+    new_list = array.join(", ")
+      new_list << ", and #{last_word}"
+    else array.size == 1 
+      array.join
+    end
+end
+
