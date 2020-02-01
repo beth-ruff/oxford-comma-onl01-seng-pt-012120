86754af63a1d3bc6b1847f1225539d28a3e400c8
Beth Ruff <beth.ruff2@gmail.com>
2020-02-01 01:58:48 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -7,12 +7,8 @@ def oxford_comma(array)
 end
 
 def oxford_common(array)
- if array.length == 2 
-  array.join(" and ")
- elsif array.length > 2 
-  array.insert(-2, "and")
-  array[0..-1].join(", ")
- else
-  array.join
-end
+  if array.length < 3 
+    return list.join(" and ")
+    
+  end
 end
