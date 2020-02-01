6195949dd629115fe34e95a4dea43d55272b42ad
Beth Ruff <beth.ruff2@gmail.com>
2020-02-01 01:56:17 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -7,5 +7,12 @@ def oxford_comma(array)
 end
 
 def oxford_common(array)
- array.to_sentence
+ if array.length == 2 
+   array.join(" and ")
+ elsif array.length > 2 
+  array.insert(-2, "and")
+  array[0..-1].join(", ")
+  else
+    array.join
+  end
 end
